dc77f4af2fe7ac7a6581a6332242e71e2045f558
julioperezrdlf@gmail.com <julioperezrdlf@gmail.com>
2022-02-04 00:42:41 +0100

    .

--- src/App.js
+++ src/App.js
@@ -1,6 +1,39 @@
 import "./App.css";
+import { useState } from "react";
+
+const api = {
+  key: "9fd288702c898e89c389cee994a75333",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
+
 
 function App() {
+
+  const dateBuilder = (d) =>{
+    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month= months[d.getMonth()];
+    let year = d.getFullYear();
+    return `${day} ${date} ${month}`
+  }
+  
+
+  const  [query, setQuery] = useState('');
+  const [weather, setWeather] = useState({});
+  const search = evt => {
+    if (evt.key === "Enter"){
+      fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+      .then(res=>res.json())
+      .then(result=>{setWeather(result);
+      setQuery('');
+    console.log(result);});
+
+    }
+  }
+
+  
   return (
     <div className="App">
       
@@ -10,6 +43,7 @@ function App() {
               type="text"
               className="inputWeather"
               placeholder="Search..."
+              autofocus
             ></input>
             <button type="submit">Search</button>
           </form>
@@ -17,16 +51,16 @@ function App() {
           <div className="infoContainer">
             <div className="infoContainer_left">
               <h1>Moscú, RU</h1>
-              <span>Longitud -3.7026 </span>
+              <span>Long -3.7026 </span>
 
-              <span>Latitud 40.4165</span>
+              <span>Lat 40.4165</span>
               <p>10-10-2022</p>
               <p>Nubes</p>
             </div>
 
             <div className="infoContainer_right">
               <p>Temperature 30.24 ºC</p>
-              <p>Sensación térmica 30.24 ºC</p>
+              <p>Feels like 30.24 ºC</p>
               <span>T min 20 </span>
               <span>T max 30</span>
               <p>Pressure 1016 hPa</p>
